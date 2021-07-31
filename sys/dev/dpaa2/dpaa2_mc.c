2c6ec6f71a8104a6debbd5a193ddfc8445195199
Dmitry Salychev <dsl@mcusim.org>
2021-07-31 13:17:55 +0200

    dpaa2: mc: Map MC portal memory as uncachable also.

--- sys/dev/dpaa2/dpaa2_mc.c
+++ sys/dev/dpaa2/dpaa2_mc.c
@@ -101,7 +101,7 @@ dpaa2_mc_attach(device_t dev)
 
 	if (sc->res[1]) {
 		resource_init_map_request(&req);
-		req.memattr = VM_MEMATTR_DEVICE;
+		req.memattr = VM_MEMATTR_DEVICE | VM_MEMATTR_UNCACHEABLE;
 		error = bus_map_resource(sc->dev, SYS_RES_MEMORY, sc->res[1],
 		    &req, &sc->map[1]);
 		if (error) {
@@ -128,7 +128,7 @@ dpaa2_mc_attach(device_t dev)
 
 	/* Map MC portal memory resource. */
 	resource_init_map_request(&req);
-	req.memattr = VM_MEMATTR_DEVICE;
+	req.memattr = VM_MEMATTR_DEVICE | VM_MEMATTR_UNCACHEABLE;
 	error = bus_map_resource(sc->dev, SYS_RES_MEMORY, sc->res[0],
 	    &req, &sc->map[0]);
 	if (error) {
