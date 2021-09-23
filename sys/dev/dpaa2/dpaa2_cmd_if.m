9616236bf3bfe06cda1d54a2580719fe605f5cd6
Dmitry Salychev <dsl@mcusim.org>
2021-09-23 23:45:40 +0200

    dpaa2: Add dpaa2_cmd and dpaa2_rc interfaces.

---
+++ sys/dev/dpaa2/dpaa2_cmd_if.m
@@ -0,0 +1,252 @@
+#-
+# SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+#
+# Copyright (c) 2021 Dmitry Salychev <dsl@mcusim.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+#include <machine/bus.h>
+#include <dev/dpaa2/dpaa2_mc.h>
+#include <dev/dpaa2/dpaa2_mcp.h>
+
+/**
+ * @brief DPAA2 MC command interface.
+ *
+ * The primary purpose of the MC provided DPAA2 objects is to simplify DPAA2
+ * hardware block usage through abstraction and encapsulation.
+ */
+INTERFACE dpaa2_cmd;
+
+#
+# Default implementation of some commands.
+#
+CODE {
+	static int
+	bypass_mng_get_version(device_t dev, dpaa2_cmd_t cmd, uint32_t *major,
+		uint32_t *minor, uint32_t *rev)
+	{
+		struct dpaa2_devinfo *dinfo;
+
+		dinfo = device_get_ivars(dev);
+		if (dinfo != NULL && dinfo->dtype == DPAA2_DEV_MC)
+			panic("no one can handle above MC");
+		if (device_get_parent(dev) != NULL)
+			return (DPAA2_CMD_MNG_GET_VERSION(device_get_parent(dev),
+				cmd, major, minor, rev));
+
+		return (ENXIO);
+	}
+};
+
+/**
+ * @brief Data Path Management (DPMNG) commands.
+ */
+
+METHOD int mng_get_version {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	*major;
+	uint32_t	*minor;
+	uint32_t	*rev;
+} DEFAULT bypass_mng_get_version;
+
+METHOD int mng_get_soc_version {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	*pvr;
+	uint32_t	*svr;
+};
+
+METHOD int mng_get_container_id {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	*cont_id;
+};
+
+/**
+ * @brief Data Path Resource Containter (DPRC) commands.
+ */
+
+METHOD int rc_open {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	 cont_id;
+	uint16_t	*token;
+};
+
+METHOD int rc_close {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int rc_get_obj_count {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	*obj_count;
+};
+
+METHOD int rc_get_obj {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	 obj_idx;
+	dpaa2_obj_t	*obj;
+};
+
+METHOD int rc_get_obj_descriptor {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	 obj_id;
+	const char	*type;
+	dpaa2_obj_t	*obj;
+};
+
+METHOD int rc_get_attributes {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	dpaa2_rc_attr_t	*attr;
+};
+
+METHOD int rc_get_obj_region {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint32_t	 obj_id;
+	uint8_t		 reg_idx;
+	const char	*type;
+	dpaa2_rc_obj_region_t *reg;
+};
+
+METHOD int rc_get_api_version {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint16_t	*major;
+	uint16_t	*minor;
+};
+
+METHOD int rc_set_irq_enable {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint8_t		 irq_idx;
+	uint8_t		 enable;
+};
+
+METHOD int rc_set_obj_irq {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	uint8_t		 irq_idx;
+	uint64_t	 addr;
+	uint32_t	 data;
+	uint32_t	 irq_usr;
+	uint32_t	 obj_id;
+	const char	*type;
+};
+
+/**
+ * @brief Data Path Network Interface (DPNI) commands.
+ */
+
+METHOD int ni_open {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	const uint32_t	 dpni_id;
+	uint16_t	*token;
+};
+
+METHOD int ni_close {
+	device_t dev;
+	dpaa2_cmd_t cmd;
+};
+
+/**
+ * @brief Data Path I/O (DPIO) commands.
+ */
+
+METHOD int io_open {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	const uint32_t	 dpio_id;
+	uint16_t	*token;
+};
+
+METHOD int io_close {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int io_enable {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int io_disable {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int io_reset {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int io_get_attributes {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	dpaa2_io_attr_t	*attr;
+};
+
+/**
+ * @brief Data Path Buffer Pool (DPBP) commands.
+ */
+
+METHOD int bp_open {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	const uint32_t	 dpbp_id;
+	uint16_t	*token;
+};
+
+METHOD int bp_close {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int bp_enable {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int bp_disable {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int bp_reset {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+};
+
+METHOD int bp_get_attributes {
+	device_t	 dev;
+	dpaa2_cmd_t	 cmd;
+	dpaa2_bp_attr_t	*attr;
+};
