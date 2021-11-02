baf01a8d8e8a8a5dd0996216e1bd8e2f311c3aaf
Dmitry Salychev <dsl@mcusim.org>
2021-11-02 19:45:11 +0100

    dpaa2: ni: Fix compilation error.

--- sys/dev/dpaa2/dpaa2_ni.c
+++ sys/dev/dpaa2/dpaa2_ni.c
@@ -639,7 +639,8 @@ setup_dpio(device_t dev, dpaa2_cmd_t cmd, uint16_t rc_token)
 
 		if (bootverbose)
 			device_printf(dev, "channel: dpio=%#jx dpcon=%#jx "
-			    "channel_id=%d\n", dpio_dev, dpcon_dev, chan_id);
+			    "channel_id=%d\n", dpio_dev, dpcon_dev,
+			    dpcon_sc->attr.chan_id);
 	}
 
 	/* TODO: De-allocate redundant DPIOs or DPCONs if exist. */
