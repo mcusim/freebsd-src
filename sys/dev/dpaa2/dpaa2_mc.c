8da1ec07ace24a8f624733edd87d49dd822745d5
Dmitry Salychev <dsl@mcusim.org>
2021-11-05 22:47:19 +0100

    dpaa2: mc: Do not manage SYS_RES_MEMORY.

--- sys/dev/dpaa2/dpaa2_mc.c
+++ sys/dev/dpaa2/dpaa2_mc.c
@@ -691,8 +691,8 @@ dpaa2_mc_rman(device_t mcdev, int type)
 	switch (type) {
 	case SYS_RES_IRQ:
 		return (&sc->msi_rman);
-	case SYS_RES_MEMORY:
-		return (&sc->io_rman);
+	/* case SYS_RES_MEMORY: */
+	/* 	return (&sc->io_rman); */
 	case DPAA2_DEV_IO:
 		return (&sc->dpio_rman);
 	case DPAA2_DEV_BP:
