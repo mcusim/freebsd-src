b6c32f917356b9a68a039d5fddbe6f59b9078ab1
Dmitry Salychev <dsl@mcusim.org>
2021-11-14 20:48:54 +0100

    Revert some local changes.

--- sys/arm64/qoriq/qoriq_dw_pci.c
+++ sys/arm64/qoriq/qoriq_dw_pci.c
@@ -181,7 +181,7 @@ qorif_dw_pci_probe(device_t dev)
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
-	device_set_desc(dev, "NXP Layerscape PCI-E Controller");
+	device_set_desc(dev, "NPX Layaerscape PCI-E Controller");
 	return (BUS_PROBE_DEFAULT);
 }
 
