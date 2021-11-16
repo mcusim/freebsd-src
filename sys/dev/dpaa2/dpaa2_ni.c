e4bf351e3cf56d4b098a1d4458ab4d3f63c24e47
Dmitry Salychev <dsl@mcusim.org>
2021-11-16 22:10:45 +0100

    dpaa2: ni: Print as "frame queues".

--- sys/dev/dpaa2/dpaa2_ni.c
+++ sys/dev/dpaa2/dpaa2_ni.c
@@ -796,7 +796,7 @@ setup_fqs(device_t dev, dpaa2_cmd_t cmd, uint16_t rc_token)
 	}
 
 	if (bootverbose)
-		device_printf(dev, "queues: tx_conf=%d rx=%d rx_error=%d\n",
+		device_printf(dev, "frame queues: tx_conf=%d rx=%d rx_err=%d\n",
 		    txc_fqs, rx_fqs, rx_err_fqs);
 
 	return (0);
