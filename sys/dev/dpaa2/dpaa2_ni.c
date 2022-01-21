a5ba7eb59d12754cec77d9de5d025ef6c1ee43c9
Dmitry Salychev <dsl@mcusim.org>
2022-01-21 12:02:09 +0100

    dpaa2: ni: Fix compilation errors.

--- sys/dev/dpaa2/dpaa2_ni.c
+++ sys/dev/dpaa2/dpaa2_ni.c
@@ -1958,6 +1958,8 @@ static int
 dpni_consume_frames(struct dpaa2_ni_channel *chan, struct dpaa2_ni_fq **src,
     uint32_t *consumed)
 {
+	struct dpaa2_io_softc *iosc = device_get_softc(chan->io_dev);
+	struct dpaa2_swp *swp = iosc->swp;
 	struct dpaa2_ni_fq *fq = NULL;
 	struct dpaa2_dq *dq;
 	struct dpaa2_fd *fd;
