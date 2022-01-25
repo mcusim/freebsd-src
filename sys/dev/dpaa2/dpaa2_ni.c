3484376020573d5f2515d266ddf0caab19c5c46e
Dmitry Salychev <dsl@mcusim.org>
2022-01-25 17:36:07 +0100

    dpaa2: ni: Comment out unused variables.

--- sys/dev/dpaa2/dpaa2_ni.c
+++ sys/dev/dpaa2/dpaa2_ni.c
@@ -2196,12 +2196,12 @@ static int
 dpni_consume_rx(struct dpaa2_ni_channel *chan, struct dpaa2_ni_fq *fq,
     struct dpaa2_fd *fd)
 {
-	device_t bp_dev;
-	struct dpaa2_ni_softc *sc = device_get_softc(chan->ni_dev);
-	struct dpaa2_bp_softc *bpsc;
-	bus_addr_t paddr = (bus_addr_t) fd->addr;
+	/* device_t bp_dev; */
+	/* struct dpaa2_ni_softc *sc = device_get_softc(chan->ni_dev); */
+	/* struct dpaa2_bp_softc *bpsc; */
+	/* bus_addr_t paddr = (bus_addr_t) fd->addr; */
 	uint8_t fd_format = ((fd->off_fmt_sl) >> 12) & 0x3;
-	int error;
+	/* int error; */
 
 	/* For debug purposes only! */
 	switch (fd_format) {
