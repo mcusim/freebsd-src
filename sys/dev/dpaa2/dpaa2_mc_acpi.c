411b5da953d3d30f6ed298ba2fd0d082ef1a091d
Dmitry Salychev <dsl@mcusim.org>
2022-04-08 17:16:08 +0200

    dpaa2: Remove "All rights reserved." statement.

--- sys/dev/dpaa2/dpaa2_mc_acpi.c
+++ sys/dev/dpaa2/dpaa2_mc_acpi.c
@@ -3,7 +3,6 @@
  *
  * Copyright (c) 2021 Dmitry Salychev <dsl@mcusim.org>
  * Copyright (c) 2021 Bjoern A. Zeeb
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
